4fd9b256de470b875933ca808a040aaec229204f
Francois Lebel <francoislebel@gmail.com>
2015-09-05 00:19:43 -0700

    Refactored script to run a callback function or executable when an ARP probe is performed by a given MAC.

--- listen_arp_requests.py
+++ listen_arp_requests.py
@@ -7,20 +7,45 @@ https://gist.github.com/eob/d2d1eec8fad32e8bcb6e
 
 from scapy.all import ARP, sniff
 
+import argparse
+import subprocess
 import time
 
 
-arp = {'10:ae:60:0e:62:47': 'bedroom'}
+parser = argparse.ArgumentParser('Runs executable when an ARP probe is performed by a given MAC.')
+parser.add_argument('mac', type=str, help='MAC address performing the ARP probe')
+parser.add_argument('executable', type=str, help='path to executable program to run when ARP probe is detected')
 
-def arp_display(pkt):
-    if pkt[ARP].op == 1: # who-has (request)
-        if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
-            if pkt[ARP].hwsrc in arp:
-                print 'Pushed %s (%s)' % (arp[pkt[ARP].hwsrc], pkt[ARP].hwsrc,)
-            else:
-                print 'ARP probe from unknown device: %s' % (pkt[ARP].hwsrc,)
 
-while True:
-    sniff(prn=arp_display, filter='arp', store=0, count=10)
-    time.sleep(1)
+class ARPListener(object):
+    def __init__(self, mac, callback=None, executable=None):
+        assert mac, 'MAC address is required.'
+        assert not all((callback, executable,)), 'One of callback or path to executable program is required.'
+        self.callback = callback
+        self.executable = executable
+        self.mac = mac.lower()
+
+    def _arp_callback(self, packet):
+        pkt = packet[ARP]
+        if pkt.op == 1 and pkt.psrc == '0.0.0.0' and pkt.hwsrc == self.mac:
+            self._run()
+
+    def _run(self):
+        if self.callback:
+            self.callback()
+        elif self.executable:
+            executable_atoms = filter(None, self.executable.split(' '))
+            subprocess.Popen(['nohup'] + executable_atoms)
+
+    def listen(self, pause_duration=1):
+        arp_callback = lambda pkt: self._arp_callback(pkt)
+        while True:
+            sniff(prn=arp_callback, filter='arp', store=0, count=10)
+            time.sleep(pause_duration)
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    listener = ARPListener(args.mac, executable=args.executable)
+    listener.listen()
 

ec750b305135c1ea974a5f7554dcbb1bd1440532
Francois Lebel <francoislebel@gmail.com>
2015-09-24 22:45:55 -0700

    Camel-cased everything.

--- internet-button.ino
+++ internet-button.ino
@@ -2,27 +2,27 @@
 
 
 InternetButton b = InternetButton();
-bool urgent_mode = false;
+bool urgentMode = false;
 
 void setup()
 {
     b.begin();
 
     Spark.function("led", led);
-    Spark.function("leds_upto", leds_upto);
+    Spark.function("leds_upto", ledsUpto);
 }
 
 void loop()
 {
    if (b.allButtonsOn()) {
-       if (urgent_mode == true) {
-           urgent_mode = false;
+       if (urgentMode == true) {
+           urgentMode = false;
            b.playNote("G5", 8);
            b.allLedsOn(255, 0, 0);
            delay(500);
            b.allLedsOff();
        }
-   } else if (urgent_mode == true) {
+   } else if (urgentMode == true) {
         for (int led = 1; led < 12; led++) {
             b.ledOff(led - 1);
             b.ledOn(led, 0, 255, 0);
@@ -39,23 +39,22 @@ void loop()
 
 int led(String command) {
     Spark.publish("Command '" + command + "' called.", NULL, 60, PRIVATE);
-    if (command == "urgent") {
-        urgent_mode = true;
-        b.playSong("E5,8,G5,8,E6,8,C6,8,D6,8,G6,8");
-        return 1;
-    }
-    else if (command == "off") {
-        urgent_mode = false;
+    if (command == "off") {
+        urgentMode = false;
         b.allLedsOff();
         return 0;
+    } else if (command == "urgent") {
+        urgentMode = true;
+        b.playSong("E5,8,G5,8,E6,8,C6,8,D6,8,G6,8");
+        return 1;
     }
     return -1;
 }
 
-int leds_upto(String led_number) {
+int ledsUpto(String ledNumber) {
     b.allLedsOff();
-    int max_number_loops = 3;
-    for (int led = 0, leds = led_number.toInt(), loops = 0; led < leds && loops <= max_number_loops; led++) {
+    int maxNumberLoops = 3;
+    for (int led = 0, leds = ledNumber.toInt(), loops = 0; led < leds && loops <= maxNumberLoops; led++) {
         if (led < 4) {
             b.ledOn(led, 0, 255, 0);
         } else if (led < 7) {
@@ -67,7 +66,7 @@ int leds_upto(String led_number) {
         }
         delay(75);
         if (led == (leds - 1) && leds > 7) {
-            if (loops < max_number_loops) {
+            if (loops < maxNumberLoops) {
                 b.allLedsOff();
             }
             led = 0;
